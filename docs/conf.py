4b3227675c49e61f9169157353fde65d1c163174
ramg-23 <118500937+ramg-23@users.noreply.github.com>
2024-02-29 10:17:24 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,6 @@ author = u"Monty Python"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_nb",
     "autoapi.extension",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
